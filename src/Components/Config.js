0d4833e3dd53868c6b3f98050826363d051a560a
Chhatrala Meet <meetchhatrala7@gmail.com>
2025-03-11 15:44:36 +0530

    new changes

--- src/Components/Config.js
+++ src/Components/Config.js
@@ -1,6 +1,6 @@
 
 // Websocket base url
-export const WS_BASE_URL = 'ws://127.0.0.1:8000'
+export const WS_BASE_URL = 'ws://127.0.0.1:8000' || 'ws://https://chat-app-backend-qzn8.onrender.com'
 
 // Images base url (Ex: User's profile pictures etc. from backend server)
-export const IMAGE_BASE_URL = 'http://127.0.0.1:8000'
+export const IMAGE_BASE_URL = 'http://127.0.0.1:8000' || 'https://chat-app-backend-qzn8.onrender.com'
